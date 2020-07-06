5013019c29869687ba4eb7ba07a982a42b96dc94
Chris Eckert <chris.eckert08@gmail.com>
2020-07-06 14:56:36 -0500

    mysql tables exercise

---
+++ albums_migration.sql
@@ -0,0 +1,14 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(50) DEFAULT 'UNKNOWN',
+    name VARCHAR(100) NOT NULL,
+    release_date INT NOT NULL,
+    sales FLOAT NOT NULL,
+    genre VARCHAR(50),
+    PRIMARY KEY(id)
+);
+
