4d83474523e2c5cbda1ab1207e03f2651633e55a
Chris Eckert <chris.eckert08@gmail.com>
2020-07-09 09:15:28 -0500

    mysql limit exercises

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -12,4 +12,4 @@ SELECT * FROM employees WHERE hire_date > '199%'
 SELECT * FROM employees WHERE hire_date > '199%'
     AND birth_date LIKE '%12-25'
     ORDER BY birth_date ASC, hire_date DESC
-    LIMIT 5 OFFSET 45
+    LIMIT 5 OFFSET 45;
